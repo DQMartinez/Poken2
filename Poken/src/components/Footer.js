422568f525bb2a9fcb4feefb353bf00ab4043623
ADSI-TARDE <ADSI-TARDE@PALDFPCBIFSD247.FormacionSena.RED>
2021-11-10 17:37:02 -0500

    first commit

---
+++ Poken/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://codealo.dev">Deiver Quinto Martinez</a>
+    </div>
+    <div>
+      <a href="https://instagram.com/codealo">Siguenos en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
